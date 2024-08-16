4f06348a0566f278a24c8f1922bb143557d31c86
Pedro Miranda <mdeapedro@gmail.com>
2024-08-16 11:21:25 -0300

    create commands

---
+++ src/commands/EmpCommand.java
@@ -0,0 +1,5 @@
+package commands;
+
+public class EmpCommand implements ICommand {
+    public void execute() { }
+}
---
+++ src/commands/ObsCommand.java
@@ -0,0 +1,5 @@
+package commands;
+
+public class ObsCommand implements ICommand {
+    public void execute() { }
+}
