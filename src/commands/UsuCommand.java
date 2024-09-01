3feea85cbcdf0612655348e6ec44f2e813b6d3f4
Pedro Miranda <mdeapedro@gmail.com>
2024-09-01 12:42:11 -0300

    update UsuCommand

--- src/commands/UsuCommand.java
+++ src/commands/UsuCommand.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import main.Loan;
 import main.LoanHistory;
-import main.LoanHistoryManager;
 import main.Output;
 import main.Sisbib;
 import users.IUser;
@@ -23,10 +23,12 @@ public class UsuCommand implements ICommand {
         Output.info(user.getName());
         Output.info();
         outputUserLoanHistories();
+        Output.info();
+        outputUserCurrentLoans();
     }
 
     private void outputUserLoanHistories() {
-        Output.info("• Histórico de Empréstimos");
+        Output.info("• Histórico de empréstimos");
 
         List<LoanHistory> userLoanHistories = Sisbib.getInstance().getLoanHistoryManager().getUserLoanHistories(user);
         if (userLoanHistories.size() == 0) {
@@ -51,8 +53,29 @@ public class UsuCommand implements ICommand {
             Output.info();
             Output.info(indexString, " ─┬─ Livro: ", loanHistory.getLoan().getCopy().getBook().getTitle());
             Output.info(indexSpacing, "  ├─ Solicitado em: ", loanCreationDate.format(formatter));
-            Output.info(indexSpacing, "  ├─ Devolução para: ", loanCreationDate.format(formatter));
+            Output.info(indexSpacing, "  ├─ Devolução para: ", loanHistory.getLoan().getReturnDate().format(formatter));
             Output.info(indexSpacing, "  └─ Devolvido em: ", loanReturnedOn.format(formatter), overdueInfo);
+            ++index;
+        }
+    }
+    
+    private void outputUserCurrentLoans() {
+        Output.info("• Empréstimos em andamento");
+        
+        List<Loan> userLoans = Sisbib.getInstance().getLoanManager().getUserLoans(user);
+        if (userLoans.size() == 0) {
+            Output.info("    (nada)");
+        }
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        int index = 1;
+        for (Loan loan : userLoans) {
+            String indexString = Integer.toString(index);
+            String indexSpacing = " ".repeat(indexString.length());
+            Output.info();
+            Output.info(indexString, " ─┬─ Livro: ", loan.getCopy().getBook().getTitle());
+            Output.info(indexSpacing, "  ├─ Solicitado em: ", loan.getCreationDate().format(formatter));
+            Output.info(indexSpacing, "  └─ Devolução para: ", loan.getReturnDate().format(formatter));
             ++index;
         }
     }
